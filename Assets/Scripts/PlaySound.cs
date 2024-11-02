1abdc38aea09d973c896ecf04c4149584b9d8e52
Mason <masonfilbrandt@gmail.com>
2024-11-02 12:51:50 -0400

    Added Sound Effects

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip sound;
+
     private void OnMouseDown()
     {
         print("Hit The Button");
+        if(sound != null)
+        {
+            AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position);
+        }
+        else
+        {
+            Debug.LogWarning("Attempted to press a sound effect button with no sound assigned");
+        }
     }
 }
