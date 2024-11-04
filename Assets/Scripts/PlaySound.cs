a21e29193c61e8329ed84dcff1c09f7dcdd15384
Mason <masonfilbrandt@gmail.com>
2024-11-04 10:34:31 -0500

    Final Touches

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -6,17 +6,55 @@ public class PlaySound : MonoBehaviour
 {
     [SerializeField]
     private AudioClip sound;
+    [SerializeField]
+    private Sprite pressedSprite;
+
+    private Sprite defaultSprite;
+    private SpriteRenderer spriteRenderer;
+
+    private AudioSource audioSource;
+
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        defaultSprite = spriteRenderer.sprite;
+    }
 
     private void OnMouseDown()
     {
         print("Hit The Button");
-        if(sound != null)
+
+        if(!isPressed)
         {
-            AudioSource.PlayClipAtPoint(sound, Camera.main.transform.position);
+            isPressed = true;
+            spriteRenderer.sprite = pressedSprite;
+
+            if(sound != null)
+            {
+                audioSource.PlayOneShot(sound);
+            }
+            else
+            {
+                Debug.LogWarning("Attempted to press a sound effect button with no sound assigned");
+            }
         }
         else
         {
-            Debug.LogWarning("Attempted to press a sound effect button with no sound assigned");
+            isPressed = false;
+            audioSource.Stop();
+            spriteRenderer.sprite = defaultSprite;
+        }
+    }
+
+    private void Update()
+    {
+        if(audioSource.isPlaying == false)
+        {
+            isPressed = false;
+            spriteRenderer.sprite = defaultSprite;
         }
     }
 }
